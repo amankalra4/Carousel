66f9666c40f93ea498356473afc5a3e1b28ad0c2
Aman <aman.kalra4@gmail.com>
2020-06-13 13:49:06 +0530

    Changes in display Text

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ function App() {
 		<Carousel>
 			<div>
 				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
-				<p className="legend">Hong Kong</p>
+				<p className="legend">This is Hong Kong</p>
 			</div>
 			<div>
 				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore"/>
-				<p className="legend">Singapore</p>
+				<p className="legend">This is Singapore</p>
 			</div>
 			<div>
 				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan"/>
-				<p className="legend">Japan</p>
+				<p className="legend">This is Japan</p>
 			</div>
 			<div>
 				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
-				<p className="legend">Las Vegas</p>
+				<p className="legend">This is Las Vegas</p>
 			</div>
 		</Carousel>
 	</div>
